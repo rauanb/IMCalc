99ecfcbc5c2ad8f10d0cf69471807e965a4fc31d
rauanb <rauan_b@hotmail.com>
2025-01-03 06:23:22 -0300

    Ajustado link footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer class="footer">
             <div>
                 <p class="footer__details">
-                    Feito com carinho pelo <a href="https://www.linkedin.com/in/rauan-liberato/">Rauan</a>
+                    Feito com carinho pelo <a target="_blank" href="https://www.linkedin.com/in/rauan-liberato/">Rauan</a>
                 </p>
             </div>
         </footer>
